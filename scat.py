f5423a6c0d079fc138aacfc8b07560e4dae2080e
Paul Ivanov <pi@berkeley.edu>
2017-07-13 16:18:34 -0500

    works again

--- scat.py
+++ scat.py
@@ -1,23 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Schrödinger's cat
 
 from __future__ import print_function
 
+import argparse
 import sys
 import random
 
 
-def print_file(f, number_lines=True, ignore_whitespace=False):
+def print_file(f, number_lines=True, ignore_blank=False):
     i = 0
     for line in f:
-        i, line_num = get_line_num(i, line, number_lines, ignore_whitespace)
+        i, line_num = get_line_num(i, line, number_lines, ignore_blank)
 
         if random.randint(0,1):
             print(line_num + line[:-1])  # ignore trailing newline
 
-def get_line_num(i, line, number_lines, ignore_whitespace):
+def get_line_num(i, line, number_lines, ignore_blank):
     if not number_lines:
         return i, ''
-    if ignore_whitespace and line == '':
+    if ignore_blank and line == '\n':
         return i, ''
     i += 1
 
@@ -25,13 +29,22 @@ def get_line_num(i, line, number_lines, ignore_whitespace):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        files = ['/dev/stdin']
-    else:
-        files = sys.argv[1:]
+    parser = argparse.ArgumentParser(description="Schrödinger's cat --"
+            "concatenate and print files")
+    parser.add_argument('files', metavar='file', nargs='*',
+            default=['/dev/stdin'], help='files')
+    parser.add_argument('-b', dest='ignore_blank', action='store_true',
+            help="Number the non-blank output lines, starting at 1.")
+    parser.add_argument('-n', dest='number_lines', action='store_true',
+            help="Number the non-blank output lines, starting at 1.")
 
-    for fname in files:
+    args = parser.parse_args()
+
+    if args.ignore_blank == True:  # -b implies -n
+        args.number_lines = True
+
+    for fname in args.files:
         with open(fname) as f:
-            print_file(f, number_lines=True, ignore_whitespace=True)
+            print_file(f, args.number_lines, args.ignore_blank)
 
 
